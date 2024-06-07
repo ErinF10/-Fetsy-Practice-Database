99d2b9e74edb0b15f3e4c0bc480371dac08259ce
Erin Forrest <144135752+ErinF10@users.noreply.github.com>
2024-06-06 18:08:04 -0700

    Update 8-seller_running_totals.sql

--- Queries/8-seller_running_totals.sql
+++ Queries/8-seller_running_totals.sql
@@ -2,20 +2,16 @@
 -- order_total. Also show a running sales total (in terms of money) for that seller over all time (hint: windowing functions) under 
 -- the column running_total. The running total should increase with each sale made.
 
-DROP PROCEDURE seller_running_totals;
 DELIMITER :
 CREATE PROCEDURE seller_running_totals(IN sellerName VARCHAR(255))
 BEGIN
-	SELECT seller_id, order_id, order_date, 
-		CONCAT('$', FORMAT(order_total, 2, 'en_US')) AS order_total,
-        CONCAT('$', FORMAT(sales_total, 2, 'en_US')) AS sales_total
+	SELECT seller_id, order_id, order_date, CONCAT('$', FORMAT(order_total, 2, 'en_US')) AS order_total, 
+		CONCAT('$', FORMAT(sales_total, 2, 'en_US')) AS sales_total
 	FROM (
-		SELECT seller_id, order_id, order_date, order_total, SUM(order_total) OVER
-			(ORDER BY order_date ROWS UNBOUNDED PRECEDING) AS sales_total
+		SELECT seller_id, order_id, order_date, order_total, SUM(order_total) OVER (ORDER BY order_date ROWS UNBOUNDED PRECEDING) AS sales_total
 		FROM (
-			SELECT (orders.order_quantity * products.product_price)/100
-				AS order_total, sellers.seller_id AS seller_id, seller_name,
-                orders.order_id AS order_id, order_date
+			SELECT (orders.order_quantity * products.product_price)/100 AS order_total, sellers.seller_id AS seller_id, 
+				seller_name, orders.order_id AS order_id, order_date
 			FROM sellers
             INNER JOIN products ON sellers.seller_id = products.seller_id
             INNER JOIN orders ON orders.product_id = products.product_id
